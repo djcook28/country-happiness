215e858edb9df4ace4385681323e249ca6ce5f79
dcook <daniel.j.cook.28@gmail.com>
2025-05-25 15:52:32 -0500

    created plotly scatter chart and cleaned up column titles to be more UI friendly

--- main.py
+++ main.py
@@ -6,12 +6,15 @@ st.title('Country Happiness Factor Correlation')
 
 df = pd.read_csv('happy.csv')
 
-columns = df.columns.to_list()
-columns.pop(0)
+df.columns = df.columns.str.replace("_", " ").str.title()
+df_columns = df.columns.to_list()
+#removes country as an option.  Countries is a label not a data point
+df_columns.pop(0)
 
-x_axis = st.selectbox('Select the data for the X-axis', options=df.columns)
+x_axis = st.selectbox('Select the data for the X-axis', options=df_columns)
 
-y_axis = st.selectbox('Select the data for the Y-axis', options=df.columns)
+y_axis = st.selectbox('Select the data for the Y-axis', options=df_columns)
 
+st.subheader(f'{x_axis} and {y_axis}')
 figure = px.scatter(data_frame=df, x=df[x_axis], y=df[y_axis])
 st.plotly_chart(figure)
